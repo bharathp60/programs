cfc5f32bee34b3dfb456d17eb13e2dadb6b0c370
bharathp <bharathp60@gmail.com>
2025-01-31 22:45:53 +0530

    [main] employee programmes

--- src/main/java/org/example/employee/EmployeeMain.java
+++ src/main/java/org/example/employee/EmployeeMain.java
@@ -2,6 +2,7 @@ package org.example.employee;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -35,14 +36,58 @@ public class EmployeeMain {
                 .forEach(System.out::println);
 
         System.out.println("Get the details of highest paid employee in the organization");
+        employeeList
+                .stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .ifPresent(System.out::println);
+
+        System.out.println("Find all the employee whose joined after 2015");
+        employeeList
+                .stream()
+                .filter(e->e.getYoj()>2015)
+                .collect(Collectors.toList())
+                .stream()
+                .forEach(System.out::println);
+
+        System.out.println("Group based on the gender and find the number of employees");
+        employeeList
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getGender,Collectors.counting()))
+                .entrySet()
+                .forEach(System.out::println);
+
+        System.out.println("Find the average salary of the each position");
+        employeeList
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getPosition, Collectors.averagingDouble(Employee::getSalary)))
+                .entrySet()
+                .forEach(System.out::println);
+
+        System.out.println("Youngest male in the developer position");
+        employeeList.stream()
+                .filter(e->e.getPosition().equalsIgnoreCase("developer"))
+                .min(Comparator.comparing(Employee::getAge))
+                .ifPresent(System.out::println);
+
+
+        System.out.println("Who as the most experience in the work");
+        String name = employeeList.stream()
+                .min(Comparator.comparing(Employee::getYoj))
+                .get().getName();
+        System.out.println("Most Experienced employee ::"+name);
+
+
+        System.out.println("Get key as an ID and name in the value in map");
+        employeeList.stream().collect(Collectors.toMap(Employee::getId,Employee::getName)).entrySet().forEach(System.out::println);
 
     }
+
 
     private static void addEmployeeList(List<Employee> employeeList) {
         System.out.println("Adding Employees in to the list");
         employeeList.add(new Employee(1,"bharath",28,"male", "developer",2018, 2334.44));
         employeeList.add(new Employee(2,"ramesh",39,"male", "tester",2019, 2879.44));
-        employeeList.add(new Employee(1,"bharath",29,"male", "developer",2018, 2334.44));
-        employeeList.add(new Employee(1,"sushmitha",47,"female", "developer",2006, 23348.44));
+        employeeList.add(new Employee(4,"bharath",29,"male", "developer",2018, 2334.44));
+        employeeList.add(new Employee(3,"sushmitha",47,"female", "developer",2006, 23348.44));
     }
 }
